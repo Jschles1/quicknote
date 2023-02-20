4a0c30ec7e4aaeaaa8ee8e21a00b9641470c347c
Jschles1 <johnschlesinger5@gmail.com>
2023-02-20 15:16:24 -0500

    Add page layout

---
+++ src/components/AppLayout.tsx
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import Navigation from './Navigation';
+
+interface AppLayoutProps {
+    children: React.ReactNode;
+}
+
+const AppLayout: React.FC<AppLayoutProps> = ({ children }) => {
+    return (
+        <main className="min-w-screen relative flex min-h-screen">
+            <Navigation />
+            <div className="container mx-auto flex max-w-7xl flex-col items-center justify-center gap-12 px-4 py-16">
+                {children}
+            </div>
+        </main>
+    );
+};
+
+export default AppLayout;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,7 @@
-import { type AppType } from 'next/app';
+import * as React from 'react';
+import { NextPage } from 'next';
+import { type AppType, AppProps } from 'next/app';
+import { Nunito } from '@next/font/google';
 import { type Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
 
@@ -6,10 +9,37 @@ import { api } from '../utils/api';
 
 import '../styles/globals.css';
 
-const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: React.ReactElement) => React.ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout;
+};
+
+const nunito = Nunito({
+    weight: ['400', '700'],
+    style: ['normal', 'italic'],
+    subsets: ['latin'],
+});
+
+const MyApp: AppType<{ session: Session | null }> = ({
+    Component,
+    pageProps: { session, ...pageProps },
+}: AppPropsWithLayout) => {
+    // Use the layout defined at the page level, if available
+    const getLayout = Component.getLayout ?? ((page) => page);
+    const Page = getLayout(<Component {...pageProps} />);
     return (
         <SessionProvider session={session}>
-            <Component {...pageProps} />
+            <>
+                <style jsx global>{`
+                    html {
+                        font-family: ${nunito.style.fontFamily};
+                    }
+                `}</style>
+            </>
+            {Page}
         </SessionProvider>
     );
 };
