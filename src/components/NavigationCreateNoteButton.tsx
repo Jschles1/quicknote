c33211b0865da3c87c8eb9130e314cecab4325db
Jschles1 <johnschlesinger5@gmail.com>
2023-02-22 16:10:50 -0500

    Create note button UI

--- src/components/NavigationCreateNoteButton.tsx
+++ src/components/NavigationCreateNoteButton.tsx
@@ -1,7 +1,17 @@
 import * as React from 'react';
+import Link from 'next/link';
+import { Plus } from 'lucide-react';
 
 const NavigationCreateNoteButton: React.FC = () => {
-    return <div className="p-4">Create Note</div>;
+    return (
+        <Link
+            href="/create-folder"
+            className="absolute bottom-0 flex w-full items-center border-t border-slate-700 p-4 decoration-0"
+        >
+            <Plus size={20} className="mr-2" />
+            <div>Create Note</div>
+        </Link>
+    );
 };
 
 export default NavigationCreateNoteButton;
