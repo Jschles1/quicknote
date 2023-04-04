d9f42b04045d094dc9d6380df57082422743539e
Jschles1 <johnschlesinger5@gmail.com>
2023-04-04 16:43:14 -0400

    Add toggle star to recently viewed notes

--- src/components/NavigationRecentlyViewedNotes.tsx
+++ src/components/NavigationRecentlyViewedNotes.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { Separator } from './ui/Separator';
 import NavigationCategoryNote from './NavigationCategoryNote';
 import { Note } from '@prisma/client';
+import useNoteType from '@/lib/hooks/use-note-type';
 
 interface Props {
     notes: Note[] | undefined;
@@ -9,13 +10,19 @@ interface Props {
 
 const NavigationRecentlyViewedNotes: React.FC<Props> = ({ notes }) => {
     if (!notes || !notes.length) return null;
+    const { mutateNoteType } = useNoteType();
+
+    const handleStarClick = async (noteId: string) => {
+        await mutateNoteType({ noteId, type: 'starred' });
+    };
+
     return (
         <>
             <Separator />
             <div className="p-4">
                 <div className="p-2 font-bold">Recently Viewed:</div>
                 {notes.map((note: any) => (
-                    <NavigationCategoryNote key={note.id} note={note} onStarClick={() => {}} />
+                    <NavigationCategoryNote key={note.id} note={note} onStarClick={() => handleStarClick(note.id)} />
                 ))}
             </div>
         </>
