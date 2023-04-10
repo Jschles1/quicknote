cec26ce7ba171defc9be3f9adc78cc82bc56ac67
Jschles1 <johnschlesinger5@gmail.com>
2023-04-10 15:31:21 -0400

    Dialog CSS change

--- src/components/ui/Dialog.tsx
+++ src/components/ui/Dialog.tsx
@@ -58,7 +58,7 @@ const DialogClose = React.forwardRef<
 >(({ className, children, ...props }, ref) => (
     <DialogPrimitive.Close
         ref={ref}
-        className="absolute top-4 right-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800"
+        className="absolute top-4 right-4 rounded-sm opacity-70 transition-opacity focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-slate-100 hover:opacity-100 dark:focus:ring-slate-400 dark:focus:ring-offset-slate-900 dark:data-[state=open]:bg-slate-800"
         {...props}
     >
         <X className="h-4 w-4" />
