49842b1f3fd2256a49f3e645a0e70596fd46848f
Jschles1 <johnschlesinger5@gmail.com>
2023-04-15 15:32:19 -0400

    Optimistic update for note types

--- src/lib/hooks/use-note-type.ts
+++ src/lib/hooks/use-note-type.ts
@@ -1,14 +1,36 @@
 import { api } from '@/utils/api';
 import { useToast } from './use-toast';
+import { Note } from '@prisma/client';
 
 function useNoteType() {
     const utils = api.useContext();
     const { toast } = useToast();
     // TODO: add optimistic update
     const { mutateAsync } = api.notes.toggleNoteType.useMutation({
-        onMutate: async () => {
-            console.log('Updating note type');
+        onMutate: async (updated) => {
+            console.log('Updating note type', updated);
             await utils.notes.getAll.cancel();
+
+            // Get the data from the queryCache
+            const prevData = utils.notes.getAll.getData() as Note[];
+
+            // Optimistically update the data with our new post
+            utils.notes.getAll.setData(undefined, (notes) =>
+                notes?.map((note) => {
+                    if (note.id === updated.noteId) {
+                        return {
+                            ...note,
+                            starred: updated.type === 'starred' ? !note.starred : note.starred,
+                            archived: updated.type === 'archived' ? !note.archived : note.archived,
+                            trash: updated.type === 'trash' ? !note.trash : note.trash,
+                        };
+                    }
+                    return note;
+                })
+            );
+
+            // Return the previous data so we can revert if something goes wrong
+            return { prevData: prevData };
         },
         onSuccess: async (data) => {
             if (data !== 'Unauthorized') {
@@ -25,7 +47,11 @@ function useNoteType() {
                 await utils.notes.invalidate();
             }
         },
-        onError: (error) => {
+        onError: (error, _, ctx) => {
+            // If the mutation fails, use the context-value from onMutate
+            if (ctx) {
+                utils.notes.getAll.setData(undefined, ctx.prevData);
+            }
             console.log('Form submission failed with error: ', error);
             toast({
                 description: 'Failed to create note! ' + error.message,
