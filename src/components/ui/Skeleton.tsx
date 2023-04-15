d7ea9dc3b7b88ec941f2e767fc0f4d717e9268a8
Jschles1 <johnschlesinger5@gmail.com>
2023-04-15 14:08:01 -0400

    Add skeleton UI component

---
+++ src/components/ui/Skeleton.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+import { cn } from '@/lib/util';
+
+export interface SkeletonProps extends React.HTMLProps<HTMLDivElement> {
+    isLoading: boolean;
+    height: number;
+    width: number;
+}
+
+const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+    ({ className, isLoading, height, width, children, ...props }, ref) => {
+        const classNameStr = cn(
+            isLoading ? `h-[${height}px] w-[${height}px] animate-pulse rounded-full bg-gray-400` : '',
+            className
+        );
+        return (
+            <div className={classNameStr} ref={ref} {...props}>
+                {children}
+            </div>
+        );
+    }
+);
+Skeleton.displayName = 'Skeleton';
+
+export { Skeleton };
